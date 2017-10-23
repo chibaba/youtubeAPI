cf718d12ab31f401865c68c8bf88eb9c9e49e4ab
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-10-23 23:59:07 +0100

    my forst commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// create a new component. this componenet should produce dome html
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+import YTSearch from "youtube-api-search";
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyA07dh4FOuvZ5_h0Xag1G4n0EbbbSaw_QU";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideos => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+          m
+        />
+      </div>
+    );
+  }
+}
+
+// take this component generated HTML and put it on the page
+
+ReactDom.render(<App />, document.querySelector(".container"));
